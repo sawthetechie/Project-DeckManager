801bff94eb3c10ff48320ee05b686d64222a6af8
SamundraNeupane <wdymsamundra@gmail.com>
2023-12-15 20:53:38 +0545

    sell functions

--- Projecting/FirstApp/src/Deckcard.jsx
+++ Projecting/FirstApp/src/Deckcard.jsx
@@ -1,18 +1,20 @@
 import Container from "./Container";
+import App from "./App";
 
 export default function Deckcard(props){
 
     const key = parseInt(props.id)
     const owneditem = props.ownedArr
     const itemsArr = props.mainArr
-
+   
     return(
         <>
         <Container className="deckCard">
             {owneditem[key] == null ?"Available Slot":<>
             <img src={itemsArr[owneditem[key]].img}/>
             <p>Name: {itemsArr[owneditem[key]].name}</p>
-            <p>Level: {itemsArr[owneditem[key]].level}</p></>}
+            <p>Level: {itemsArr[owneditem[key]].level}</p>
+            <button onClick={props.onClick} className="btn-default" >Sell</button></>}
         </Container>
         </>
     )
